11261d6460b3a05345331937f392504573c6d2bc
usmanhassannaqvi <usmanhassannaqvi@gmail.com>
2023-02-24 17:24:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[myInput,setMyInput]=useState("")
+  const[todoList, setTodoList]=useState([])
+
+
+  const handleInput=(event)=>{
+    setMyInput(event.target.value)
+
+  }
+  const handleTask=()=>{
+    const task={
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: myInput,
+      completed:false
+    }
+   const newList=[...todoList,task]
+   setTodoList(newList)
+  }
+
+  const deleteTask=(id)=>{
+    const newList =todoList.filter((task)=>{
+      if (task.id===id){
+        return false
+      }else{
+        return true
+      }
+    })
+    setTodoList(newList)
+  }
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleInput}/>
+      <button onClick={handleTask}>Add Task</button> 
+      <div>
+        {todoList.map((task)=>{
+          return (
+            <div style={{display:"flex", alignItems:"center", gap:"10px ", justifyContent:"center",backgroundColor: task.completed ? "green" : "white"}}>
+            <h1>{task.taskName}</h1>
+            <button onClick={()=>completeTask(task.id)}>Completed</button>
+            <button onClick={()=>deleteTask(task.id)}>x</button>
+            <h1>{task.completed}</h1>
+            </div>
+          )
+        })}
+      </div>
+       
     </div>
+   
   );
 }
 
